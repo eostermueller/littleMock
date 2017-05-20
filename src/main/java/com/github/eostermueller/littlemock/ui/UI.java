9a2b5dfb091def5ce7cbee171fb08fe74514ba76
Erik Ostermueller <eostermueller@gmail.com>
2017-05-20 16:13:06 -0500

    ui buttons now have labels

--- src/main/java/com/github/eostermueller/littlemock/ui/UI.java
+++ src/main/java/com/github/eostermueller/littlemock/ui/UI.java
@@ -14,18 +14,32 @@ public class UI {
 
 	// inject via application.properties
 	@Value("${xpath.message:test}")
-	private String xpathMessage = "Hello World";
-	@Value("${other.message:test}")
-	private String otherMessage = "Hello World";
-	@Value("${busy.message:test}")
-	private String busyMessage = "Hello World";
+	private String xpathMessage = "XPath Optimizations";
 	
+	@Value("${other.message:test}")
+	private String otherMessage = "Other Optimizations";
+	
+	@Value("${busy.message:test}")
+	private String busyMessage = "Busy Processing Optimizations";
+	
+	@Value("${busy.description:test}")
+	private String busyDescription = "Larger #'s = more processing, less performance.  X * Y = number of iterations of random number generation and getter/setter activiy.";
+	
+	@Value("${busy.update:test}")
+	private String busyUpdate = "Update";
+		
+	@Value("${busy.sleep:test}")
+	private String busySleep = "Time in milliseconds to Thread.sleep().";
+		
     @RequestMapping("/ui")
     public String optimizations(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         model.addAttribute("xpathMessage", this.xpathMessage);
         model.addAttribute("otherMessage", this.otherMessage);
         model.addAttribute("busyMessage", this.busyMessage);
+        model.addAttribute("busyDescription", this.busyDescription);
+        model.addAttribute("busyUpdate", this.busyUpdate);
+        model.addAttribute("busySleep", this.busySleep);
         return "optimizations";
     }
 
